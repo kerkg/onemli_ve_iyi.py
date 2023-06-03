85789e15fd7dc78a7fef31aa360e6b55e83f280c
kerkg <134922669+kerkg@users.noreply.github.com>
2023-06-03 08:54:22 +0300

    Update index_error_overrider(İEO)v0.1.py
    while teknolojisiyle donatılmış index out of range hatasına son veren bu muazzam uygulamaya bayılacaksınız.

--- index_error_overrider(İEO)v0.1.py
+++ index_error_overrider(İEO)v0.1.py
@@ -1,3 +1,4 @@
+#index out of range hatasını almaktan bıktınızmı o zaman İEO şizin çözümünüzdür.Stringlerde fazla indexi döngü kurarak kullanan bu uygulamaya bayılacaksınız.
 text=input("birşey yaz:")
 text_len=len(text)
 user_in=int(input("index no giriniz:"))
@@ -5,6 +6,6 @@ if text_len<user_in:
     while text_len<user_in:
         user_in-=text_len
         print(user_in)
-    print(text[user_in-1])#a
+    print(text[user_in-1])
 else:
-    print(text[user_in-1])#b
+    print(text[user_in-1])
