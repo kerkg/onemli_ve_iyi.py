83e4e91b906e278235d2eb18cb7a92a1fa9ad70c
kerkg <134922669+kerkg@users.noreply.github.com>
2023-06-13 19:23:35 +0300

    Update konum_atayıcı.py
    bu modül rastgele konum bulmanızı sağlar

--- konum_atayıcı.py
+++ konum_atayıcı.py
@@ -1,4 +1,5 @@
 #x=680,y=350-->sınır
+#bu mükemmel modül ultra mükemmel random modülünün mükemmelleşmiş randint yöntemiyle çalışıp turtle haritanızda rastgele yer bulmanızı sağlayan x,y listesini verir.
 import random
 def rastgele_konum_Bulucu():
     x=random.randint(-680,680)
