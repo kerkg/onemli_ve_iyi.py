be032b873cc46693ba414e454fbc14f2a27cef57
kerkg <134922669+kerkg@users.noreply.github.com>
2023-06-15 15:16:57 +0300

    Add files via upload

---
+++ mükemmelliyetçi_phyton.py
@@ -0,0 +1,12 @@
+user=input("kimsin sen:")
+string=input("düşünceni şöyle ama bir harfi çok fazla kullanmamaya dikkat et\n:")
+mükemmel_bool=1
+for i in range(len(string)):
+    harf=string[i]
+    harf_sayısı=string.count(harf)
+    if harf_sayısı>=3:
+        print(f"cümlelerin de çok fazla harf tekrarı var o yüzden senden kurtulmalıyız\n/<phyton>:/*/break({user})\n<phyton> {user} has terminated")
+        mükemmel_bool=0
+        break
+if bool(mükemmel_bool):
+    print("cümlen mükemelliyetçi  o zaman iyisin")
