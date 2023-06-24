6605dd58f3eb161a3a37a7bdc46b14cfb8ff3816
kerkg <134922669+kerkg@users.noreply.github.com>
2023-06-24 09:01:45 +0300

    Update list_to_str.py

--- list_to_str.py
+++ list_to_str.py
@@ -1,3 +1,4 @@
+#str lerdeki replace teknolojisini kullanarak listenizi silen ve yerine str veren bu mükemmel uygulamaya bayılacaksınız.
 ör_list=list("bu bir örnek listedir ve isterseniz bu kısmı çıkarıp kendi listenizi takabilirsiniz")
 ör_str=str(ör_list)
 ör_str=ör_str.replace("[","")
