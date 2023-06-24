1d191b07add0c819bbe332dce4810924e4676f8b
kerkg <134922669+kerkg@users.noreply.github.com>
2023-06-24 08:58:51 +0300

    Add files via upload

---
+++ list_to_str.py
@@ -0,0 +1,7 @@
+ör_list=list("bu bir örnek listedir ve isterseniz bu kısmı çıkarıp kendi listenizi takabilirsiniz")
+ör_str=str(ör_list)
+ör_str=ör_str.replace("[","")
+ör_str=ör_str.replace("]","")
+ör_str=ör_str.replace("'","")
+ör_str=ör_str.replace(",","")
+print(ör_str)                                                                                                     # puts ör_str
