7ee2ee39c31afb3f7512f141b80f2e0e17690918
kerkg <134922669+kerkg@users.noreply.github.com>
2023-07-16 08:55:32 +0300

    Update faktotiyel_bulucu.py

--- faktotiyel_bulucu.py
+++ faktotiyel_bulucu.py
@@ -1,7 +1,8 @@
+#sınavda faktöriyel sorusu mu çıktı hiç telaşlanmayın çünkü bu program faktoriyel hesaplamada çok hızlı artık phyton interpreter la açar mısınız sınavdan 0 mı alırsınız o size kalmış
 def faktoriyel_bulucu(num:int):
     num2=1
     for i in range(1,num+1):
         num2*=i
     return num2
-print(faktoriyel_bulucu(10))
+#print(faktoriyel_bulucu(10))
         
